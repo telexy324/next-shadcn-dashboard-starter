86824f2580ed30dee3e51d1b54a5104274321367
Yang Xu <yangxu@YangdeMacBook-Pro.local>
2024-02-26 04:15:49 +0800

    2024022601

---
+++ app/(auth)/(signin)/layout.tsx
@@ -0,0 +1,64 @@
+import Header from "@/components/layout/header";
+import Sidebar from "@/components/layout/sidebar";
+import type {Metadata} from "next";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
+import {buttonVariants} from "@/components/ui/button";
+import UserAuthForm from "@/components/forms/user-auth-form";
+
+export const metadata: Metadata = {
+  title: "Next Shadcn Dashboard Starter",
+  description: "Basic dashboard with Next.js and Shadcn",
+};
+
+export default function DashboardLayout({
+                                          children,
+                                        }: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <div
+        className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          href="/examples/authentication"
+          className={cn(
+            buttonVariants({variant: "ghost"}),
+            "absolute right-4 hidden top-4 md:right-8 md:top-8",
+          )}
+        >
+          Login
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900"/>
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"/>
+            </svg>
+            Logo
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than ever
+                before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
+          </div>
+        </div>
+        <main className="p-4 lg:p-8 h-full flex items-center">{children}</main>
+      </div>
+    </>
+  );
+}
