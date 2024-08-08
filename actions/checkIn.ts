b95d9d14f6c25aefc6f80334eb011df7c2d5a3f4
Yang Xu <yangxu@YangdeMacBook-Pro.local>
2024-08-09 05:59:00 +0800

    2024080901

--- actions/checkIn.ts
+++ actions/checkIn.ts
@@ -3,6 +3,7 @@
 import * as z from "zod";
 import { CheckInSchema, CheckInsSchema } from "@/schemas";
 import { db } from "@/lib/db";
+import { CheckInRecord } from "@prisma/client";
 
 export const checkIn = async (values: z.infer<typeof CheckInSchema>) => {
   const validatedFields = CheckInSchema.safeParse(values);
@@ -37,25 +38,28 @@ export const checkInDelete = async (id: string) => {
 
 export const checkIns = async (
   values: z.infer<typeof CheckInsSchema>,
-): Promise<db.checkInRecord[]> => {
-  const validatedFields = CheckInsSchema.safeParse(values);
+): Promise<CheckInRecord[]> => {
+  try {
+    const validatedFields = CheckInsSchema.safeParse(values);
 
-  if (!validatedFields.success) {
-    return { error: "Invalid fields!" };
+    if (!validatedFields.success) {
+      return { error: "Invalid fields!" };
+    }
+
+    const { pagination, checkIn } = validatedFields.data;
+    const direction = pagination.desc === true ? "desc" : "asc";
+
+    return await db.checkInRecord.findMany({
+      orderBy: pagination.orderBy
+        ? {
+            [pagination.orderBy]: direction,
+          }
+        : {},
+      skip: (pagination.page - 1) * pagination.limit, // 计算要跳过的记录数，实现分页
+      take: pagination.limit, // 每页返回的记录数
+    });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error; // 抛出错误，让调用方处理
   }
-
-  const { pagination, checkIn } = validatedFields.data;
-  const direction = pagination.desc === true ? "desc" : "asc";
-
-  await db.checkInRecord.findMany({
-    orderBy: pagination.orderBy
-      ? {
-          [pagination.orderBy]: direction,
-        }
-      : {},
-    skip: (pagination.page - 1) * pagination.limit, // 计算要跳过的记录数，实现分页
-    take: pagination.limit, // 每页返回的记录数
-  });
-
-  return { success: "Check in recorded!" };
 };
